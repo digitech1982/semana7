401cf1ce0448b295b2614a3bf94197396fcb3956
DIEGO PORTILLA <dijapm@outlook.com>
2022-05-23 00:44:37 -0500

    Agregar archivos de proyecto.

---
+++ semana7/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace semana7
+{
+    public interface Database
+    {
+        SQLiteAsyncConnection GetConnection();//Implementacion en cada proyecto Android,iOS
+
+    }
+}
